a49d8b2e6ae327fad9934ed49fb48b19b6c7906d
vanhoan01 <92982928+vanhoan01@users.noreply.github.com>
2022-09-18 01:45:37 +0700

    BT Class

--- BTClass/sinhvien.rb
+++ BTClass/sinhvien.rb
@@ -3,6 +3,35 @@
 # Tạo phương thức nhập điểm Toán Lý Hoá của sinh viên, tính điểm trung bình
 # In ra các thông tin của sinh viên
 
-class sinhvien
-    puts 'Hello!'
-end
+class SinhVien
+    def initialize(msv, hovaten, diachi)
+       @sv_msv = msv
+       @sv_hovaten = hovaten
+       @sv_diachi = diachi
+    end
+
+    def nhapDiem_dtb
+        print "Nhap diem toan: "
+        @sv_toan = gets.to_f
+        print "Nhap diem ly: "
+        @sv_ly = gets.to_f
+        print "Nhap diem hoa: "
+        @sv_hoa = gets.to_f
+        $sv_dtb = (@sv_toan + @sv_ly + @sv_hoa)/3
+    end
+
+    def thongTin
+        puts("Ma sinh vien: #{@sv_msv}")
+        puts("Ho va ten: #{@sv_hovaten}")
+        puts("Dia chi: #{@sv_diachi}")
+        puts("Diem Toan: #{@sv_toan}")
+        puts("Diem Ly: #{@sv_ly}")
+        puts("DIem Hoa: #{@sv_hoa}")
+        puts("Diem trung binh: #{$sv_dtb}")
+    end
+ end
+
+ sv = SinhVien.new("SV001", "Nguyễn Văn Hoàn", "Hải Châu, Đà Nẵng")
+ sv.nhapDiem_dtb
+ puts("\nThong tin sinh vien:")
+ sv.thongTin
