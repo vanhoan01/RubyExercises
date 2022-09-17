931cfd7f55ad7ce9d334a1bfb4408a65d4ee6445
vanhoan01 <92982928+vanhoan01@users.noreply.github.com>
2022-09-17 23:06:41 +0700

    bt class

---
+++ BTClass/sinhvien.rb
@@ -0,0 +1,8 @@
+# Tạo class sinhvien
+# Thiết kế các phương thức cơ bản của class
+# Tạo phương thức nhập điểm Toán Lý Hoá của sinh viên, tính điểm trung bình
+# In ra các thông tin của sinh viên
+
+class sinhvien
+    puts 'Hello!'
+end
